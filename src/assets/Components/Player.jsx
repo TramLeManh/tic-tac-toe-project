d460c58b09b37d095c230dd295611fd8bffd95ed
Manh <ITITIU21077@student.hcmiu.edu.vn>
2025-04-07 17:43:42 +0700

    EditPlayerName_V1

--- src/assets/Components/Player.jsx
+++ src/assets/Components/Player.jsx
@@ -2,17 +2,30 @@ import React, { useState } from 'react'
 export default function Player({ playerName, symbol }) {
     const [isEdit, setEdit] = useState(false);
     const [name,setName] = useState(playerName)
-    let text = <span className="player-name">{name}</span>
+    let text = <span className="player-name font-[1000] bg-sky-500/10">{name}</span>
     if (isEdit) {
-        text = <input id="playerName"  type='text' required defaultValue={name} ></input>
+        // text = <input id="playerName" className="playerName" type='text' required defaultValue={name} ></input>
+
+        text = <input 
+            id="playerName" 
+            className="playerName" 
+            type='text' 
+            required 
+            defaultValue={name} 
+            autoFocus 
+            onBlur={() => {changeName()}} 
+        ></input>
     }
     function handleClick() {
         if(isEdit){
-            setName(document.getElementById('playerName').value);
+            changeName()
         }
         return setEdit(isEdit=>!isEdit);   
     }
-
+    function changeName(){
+        setName(document.getElementById('playerName').value);
+        text = <span className="player-name font-[1000] bg-sky-500/10">{name}</span>
+    }
     return (
         <li>
             {text}
