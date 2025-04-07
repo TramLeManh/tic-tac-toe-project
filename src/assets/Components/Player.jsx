3b70b635c8e16fe41048c2c0d69890832abf40c0
Manh <ITITIU21077@student.hcmiu.edu.vn>
2025-04-07 17:44:19 +0700

    EditPlayerName_V2

--- src/assets/Components/Player.jsx
+++ src/assets/Components/Player.jsx
@@ -1,36 +1,29 @@
-import React, { useState } from 'react'
-export default function Player({ playerName, symbol }) {
-    const [isEdit, setEdit] = useState(false);
-    const [name,setName] = useState(playerName)
-    let text = <span className="player-name font-[1000] bg-sky-500/10">{name}</span>
-    if (isEdit) {
-        // text = <input id="playerName" className="playerName" type='text' required defaultValue={name} ></input>
+import React, { useState } from "react";
 
-        text = <input 
-            id="playerName" 
-            className="playerName" 
-            type='text' 
-            required 
-            defaultValue={name} 
-            autoFocus 
-            onBlur={() => {changeName()}} 
-        ></input>
+export default function Player({ playerName, symbol }) {
+    const [name, setName] = useState(playerName);
+    const [isEdit, setEdit] = useState(false);
+
+    function handleBlur(event) {
+        setName(event.target.value);
+        setEdit(false);
     }
-    function handleClick() {
-        if(isEdit){
-            changeName()
-        }
-        return setEdit(isEdit=>!isEdit);   
-    }
-    function changeName(){
-        setName(document.getElementById('playerName').value);
-        text = <span className="player-name font-[1000] bg-sky-500/10">{name}</span>
-    }
+
     return (
         <li>
-            {text}
+            {isEdit ? (
+                <input
+                    type="text"
+                    required
+                    defaultValue={name}
+                    autoFocus
+                    onBlur={handleBlur}
+                />
+            ) : (
+                <span className="player-name font-[1000] bg-sky-500/10">{name}</span>
+            )}
             <span className="player-symbol">{symbol}</span>
-            <button onClick={handleClick}>Edit</button>
+            <button onClick={() => setEdit(!isEdit)}>Edit</button>
         </li>
-    )
+    );
 }
