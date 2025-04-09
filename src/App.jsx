3d8a805e807782ad15a6f52a2947c2604ea42fc4
Manh <ITITIU21077@student.hcmiu.edu.vn>
2025-04-09 22:32:32 +0700

    refactor: :zap: Make Function App
    - Make funcitons to handle variables . Optimize code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,42 @@ import GameBoard from "./assets/Components/GameBoard";
 import Player from "./assets/Components/Player";
 import Log from "./assets/Components/log";
 import { WINNING_COMBINATIONS } from "./data/Win";
-import GameOver from "./assets/Components/GameOver"
+import GameOver from "./assets/Components/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function App() {
-  //Define state object
-  //Player Components and GameOver components will use it
-  const [playerName, setPlayerName] = useState({
-    O: "Player1",
-    X: "Player2"
-  });  
-  // const [activePlayer, setActivePlayer] = useState("X");
-  const [gameTurn, setgameTurn] = useState([]);
-  const activePlayer = handlePlayer(gameTurn);
-  const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
-  let gameBoard = [...initialGameBoard.map(array=>[...array])];
-  let winner;
-  var hasDraw
-  for (const t of gameTurn) {
-    const { row, col } = t.square;
-    gameBoard[row][col] = t.player;
+  function deriveGameBoard(gameTurn) {
+    let gameBoard = [...initialGameBoard.map((array) => [...array])];
+    for (const t of gameTurn) {
+      const { row, col } = t.square;
+      gameBoard[row][col] = t.player;
+    }
+    return gameBoard;
+  }
+  function deriveWinner(gameBoard, players) {
+    let winner;
+    for (const w of WINNING_COMBINATIONS) {
+      const first = gameBoard[w[0].row][w[0].column];
+      const second = gameBoard[w[1].row][w[1].column];
+      const third = gameBoard[w[2].row][w[2].column];
+      if (first && (first === second) & (second === third)) {
+        winner = players[first];
+      }
+    }
+    return winner;
   }
   //Spread object
   function handlePlayerName(symbol, name) {
     setPlayerName((prev) => ({
       ...prev,
-      [symbol]: name
+      [symbol]: name,
     }));
   }
-  
   function handleSelectSquare(rowIndex, colIndex) {
     setgameTurn((prev) => {
       let currentPlayer = handlePlayer(prev);
@@ -44,9 +49,8 @@ function App() {
       return updateTurns;
     });
   }
-  function handleRestart(){
-    setgameTurn([])
-    winner=hasDraw=null
+  function handleRestart() {
+    setgameTurn([]);
   }
   function handlePlayer(gameTurn) {
     let currentPlayer = "X";
@@ -55,49 +59,52 @@ function App() {
     if (gameTurn.length > 0 && gameTurn[0].player === "X") {
       currentPlayer = "O";
     }
-    if(gameTurn.length===9){
-      hasDraw=true
-    }
+
     return currentPlayer;
   }
-  for (const w of WINNING_COMBINATIONS) {
-    const first = gameBoard[w[0].row][w[0].column]
-    const second = gameBoard[w[1].row][w[1].column]
-    const third = gameBoard[w[2].row][w[2].column]
-    if (first && first === second & second === third) {
-      winner = gameTurn[0].player
-    }
-  }
+  
+  //Define state object
+  //Player Components and GameOver components will use it
+  const [playerName, setPlayerName] = useState({
+    X: "Player1",
+    O: "Player2",
+  });
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurn, setgameTurn] = useState([]);
+  const activePlayer = handlePlayer(gameTurn);
+  const gameBoard = deriveGameBoard(gameTurn);
+  const winner = deriveWinner(gameBoard, playerName);
+  const hasDraw = gameTurn.length === 9 && !winner;
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          //Put func  handlePlayerName from parent to child
+          {/* Put func handlePlayerName from parent to child */}
           <Player
-            playerName={"Player1"}
+            playerName={playerName.X}
             symbol={"X"}
             isActive={activePlayer === "X"}
             handlePlayerName={handlePlayerName}
           ></Player>
           <Player
-            playerName={"Player2"}
+            playerName={playerName.O}
             symbol={"O"}
             isActive={activePlayer === "O"}
-            handlePlayerName={handlePlayerName}            
+            handlePlayerName={handlePlayerName}
           ></Player>
         </ol>
-        //Put player name win it this code
-        {(winner||hasDraw)&&<GameOver winner={playerName[winner]} onRestart={handleRestart}></GameOver>}
+        {/* Put player name win it this code */}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart}></GameOver>
+        )}
         <GameBoard
-        onSelectSquare={handleSelectSquare}
-        playerSymbol={activePlayer}
-        GameBoard={gameBoard}
-      ></GameBoard>
-
+          onSelectSquare={handleSelectSquare}
+          playerSymbol={activePlayer}
+          GameBoard={gameBoard}
+        ></GameBoard>
       </div>
-      
-      
-      
+
       <Log turns={gameTurn}></Log>
     </main>
   );
